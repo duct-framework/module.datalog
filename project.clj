43f7f46fed29b4ac235d4c5c044c2362dc9ddbc4
James Reeves <jreeves@weavejester.com>
2026-08-05 15:02:00 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.5"]
                  [org.duct-framework/database.datalog "0.2.1"]
-                 [org.duct-framework/repl.refers "0.1.0"]
+                 [org.duct-framework/repl.refers "0.1.1"]
                  [org.duct-framework/file.temp "0.1.0"]
                  [integrant "1.0.1"]]
   :profiles
   {:dev {:dependencies [[org.duct-framework/database.datalog.datalevin "0.1.3"]
-                        [integrant/repl "0.5.0"]]}})
+                        [integrant/repl "0.5.1"]]}})
